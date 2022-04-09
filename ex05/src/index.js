3e1f5e2914d945d5baacfe28e4a2dc31cc17e3f2
ana-brdar <brdar.ana@yahoo.com>
2022-04-09 06:26:09 +0200

    Nested arrays

---
+++ ex05/src/index.js
@@ -0,0 +1,24 @@
+
+var myPetsArray = [
+    {
+        animalType: "Dog",
+        name: "Pujdo",
+    },
+    {
+        animalType: "Cat",
+        name: "Maca",
+    },
+    {
+        animalType: "Bird",
+        name: "Tweety"
+    }
+]
+
+function myPetsFunction(pets) {
+    pets = myPetsArray[1].name;
+    return pets;
+}
+
+console.log(myPetsFunction(myPetsArray));
+module.exports = myPetsFunction;
+module.exports.myPets = myPetsArray;
